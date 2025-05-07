c77a1239db3ed620419b395484b984a0c91dfe57
Gnoddeno <82995859+gnoddeno@users.noreply.github.com>
2025-05-07 18:31:26 +0900

    Update settings.py

--- ICU/ICU/settings.py
+++ ICU/ICU/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-($8=pz3prorq&7ug+pud*k-tk%5$vv3&moxrq@zgmx7@d-y=t&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
